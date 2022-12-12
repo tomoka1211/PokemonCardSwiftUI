460d04204376d964a169e335b8505c5dcfd39a03
Hirano Tomoka <kamui1211@gmail.com>
2022-12-12 13:57:30 +0900

    Initial Commit

---
+++ PokemonCardSwiftUI/PokemonCardSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonCardSwiftUIApp.swift
+//  PokemonCardSwiftUI
+//
+//  Created by hirano_tomoka on 2022/12/12.
+//
+
+import SwiftUI
+
+@main
+struct PokemonCardSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
